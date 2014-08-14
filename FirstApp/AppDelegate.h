dab1950bd78c302070eb8babb7a5e088a2d6358e
DONGMIN LEE <reoim@192-168-1-9.tpgi.com.au>
2014-08-14 14:42:30 +1000

    Initial Commit

---
+++ FirstApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FirstApp
+//
+//  Created by DONGMIN LEE on 14/08/2014.
+//  Copyright (c) 2014 Dongmin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
